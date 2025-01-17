7526be300a9e18d07d338044eb9bbd76063c100c
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-01-17 21:39:51 +0330

    feat: add swagger

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/gateway.module';
 import { Logger } from '@nestjs/common';
+import { swaggerConfigInit } from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('NestApplication')
 
   const { PORT = 4000 } = process.env
+
+  swaggerConfigInit(app)
 
   await app.listen(PORT);
   logger.log(`Application running on port ${PORT}`)
