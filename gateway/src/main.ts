64d2458342d5ee8fe981bc621195a6fdb351b89a
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-01-17 20:48:56 +0330

    feat: add logger in main file

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/gateway.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('NestApplication')
+
+  const { PORT = 4000 } = process.env
+
+  await app.listen(PORT);
+  logger.log(`Application running on port ${PORT}`)
 }
 bootstrap();
