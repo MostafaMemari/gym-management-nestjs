53d8bda5e41ca9f367d563f4a5c4989cf928b133
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-01-18 23:34:18 +0330

    feat: add rabbitmq options

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { RmqOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL],
+      queue: process.env.RABBITMQ_QUEUE_NAME
+    }
+  } as RmqOptions);
+
+  const logger = new Logger("NestApplication")
+
+  await app.listen()
+  logger.log("Auth service is running....")
 }
 bootstrap();
