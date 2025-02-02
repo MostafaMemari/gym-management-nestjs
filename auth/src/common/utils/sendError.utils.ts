e62ecbf2a437bcbb6da68d33924fd98c28b08e9c
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-02-02 12:20:44 +0330

    feat: add sendError fn

---
+++ auth/src/common/utils/sendError.utils.ts
@@ -0,0 +1,23 @@
+import { HttpStatus } from "@nestjs/common";
+import { ServiceResponse } from "../../interfaces/serviceResponse.interface";
+
+const extractErrorMessage = (error: any, defaultMessage?: string) => {
+    if (typeof error == 'string')
+        return error
+    else if (typeof error == 'object' && error?.message)
+        return error.message
+
+    return defaultMessage || ""
+}
+
+export const sendError = (error: any): ServiceResponse => {
+    const authServiceMessage = "Auth Service Internal Server Error !!"
+    const errorMessage = extractErrorMessage(error, authServiceMessage)
+
+    return {
+        error: true,
+        data: {},
+        message: errorMessage,
+        status: HttpStatus.INTERNAL_SERVER_ERROR
+    }
+}
