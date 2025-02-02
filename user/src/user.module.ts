595d70b3006113eb2b18a1a08eb669af1ce5eda0
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-02-02 21:06:06 +0330

    feat: added prisma module and config

--- user/src/user.module.ts
+++ user/src/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import envConfig from './configs/env.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(envConfig())
+    ConfigModule.forRoot(envConfig()),
+    PrismaModule
   ],
   controllers: [UserController],
   providers: [UserService],
