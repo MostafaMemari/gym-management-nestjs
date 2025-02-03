588667a339c0f312201391470acb2db63b039bc3
MostafaMemari <m.memari7@yahoo.com>
2025-02-03 15:15:20 +0330

    ✨ feat: create service student

--- gateway/src/app/gateway.module.ts
+++ gateway/src/app/gateway.module.ts
@@ -1,15 +1,80 @@
 import { Module, ValidationPipe } from "@nestjs/common";
-
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { Services } from "../common/enums/services.enum";
 import { AuthController } from "./controllers/auth.controller";
 import { ConfigModule } from "@nestjs/config";
 import envConfig from "../configs/env.config";
 import { UserController } from "./controllers/user.controller";
 import { PermissionController } from "./controllers/permission.controller";
 import { APP_PIPE } from "@nestjs/core";
-import { RmqClientsModule } from "./rmq-clients.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(envConfig()), RmqClientsModule],
+  imports: [
+    ConfigModule.forRoot(envConfig()),
+
+    ClientsModule.register([
+      {
+        name: Services.AUTH,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: process.env.RABBITMQ_AUTH_SERVICE_QUEUE,
+          prefetchCount: 2,
+          isGlobalPrefetchCount: true,
+          noAck: true,
+          persistent: false,
+        },
+      },
+      {
+        name: Services.USER,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: process.env.RABBITMQ_USER_SERVICE_QUEUE,
+          prefetchCount: 2,
+          isGlobalPrefetchCount: true,
+          noAck: true,
+          persistent: false,
+        },
+      },
+      {
+        name: Services.PERMISSION,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: process.env.RABBITMQ_PERMISSION_SERVICE_QUEUE,
+          prefetchCount: 2,
+          isGlobalPrefetchCount: true,
+          noAck: true,
+          persistent: false,
+        },
+      },
+      {
+        name: Services.REDIS,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: process.env.RABBITMQ_REDIS_SERVICE_QUEUE,
+          prefetchCount: 2,
+          isGlobalPrefetchCount: true,
+          noAck: true,
+          persistent: false,
+        },
+      },
+      {
+        name: Services.STUDENT,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: process.env.RABBITMQ_REDIS_SERVICE_QUEUE,
+          prefetchCount: 2,
+          isGlobalPrefetchCount: true,
+          noAck: true,
+          persistent: false,
+        },
+      },
+    ]),
+  ],
   controllers: [AuthController, UserController, PermissionController],
   providers: [
     {
