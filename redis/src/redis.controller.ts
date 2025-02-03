6d74ff1262a44b76ed348e741f927510678110e3
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-02-03 23:47:06 +0330

    feat: add set method

--- redis/src/redis.controller.ts
+++ redis/src/redis.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { RedisPatterns } from './enums/redis.events';
+import { ISet } from './interfaces/redis.interface';
 
 @Controller()
 export class RedisController {
@@ -10,6 +11,11 @@ export class RedisController {
   @MessagePattern(RedisPatterns.Get)
   get(@Payload() data: { key: string }) {
     return this.redisService.get(data)
+  }
+
+  @MessagePattern(RedisPatterns.Set)
+  set(@Payload() data: ISet) {
+    return this.redisService.set(data)
   }
 
   @MessagePattern(RedisPatterns.CheckConnection)
--- redis/src/redis.service.ts
+++ redis/src/redis.service.ts
@@ -1,25 +1,43 @@
 import { InjectRedis } from '@nestjs-modules/ioredis';
 import { HttpStatus, Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
+import { RedisMessages } from './enums/redis.messages';
+import { ISet } from './interfaces/redis.interface';
+import { ServiceResponse } from './interfaces/serviceResponse.interface';
 
 @Injectable()
 export class RedisService {
     constructor(@InjectRedis() private readonly redis: Redis) { }
 
-    async get(data: { key: string }) {
+    async get(data: { key: string }): Promise<ServiceResponse> {
         const value = await this.redis.get(data.key)
 
         if (!value) {
             return {
                 data: {},
                 error: true,
-                message: "",
+                message: RedisMessages.NotFound,
                 status: HttpStatus.NOT_FOUND
             }
         }
 
         return {
-            data: { value }
+            data: { value },
+            error: false,
+            message: "",
+            status: HttpStatus.OK
         }
     }
+
+    async set(data: ISet): Promise<ServiceResponse> {
+        const value = await this.redis.set(data.key, data.value, "EX", data.expireTime)
+
+        return {
+            data: {},
+            error: false,
+            message: value,
+            status: HttpStatus.CREATED
+        }
+    }
+
 }
