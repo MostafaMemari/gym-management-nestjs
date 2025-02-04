8fbdf04db353f13cd8de4478db47a42d89951476
MostafaMemari <m.memari7@yahoo.com>
2025-02-04 14:40:22 +0330

    ✨ feat: add .prettierrc file

--- gateway/src/app/controllers/student.controller.ts
+++ gateway/src/app/controllers/student.controller.ts
@@ -1,15 +1,15 @@
-import { Body, Controller, Get, HttpException, Inject, InternalServerErrorException, Post } from "@nestjs/common";
-import { Services } from "../../common/enums/services.enum";
-import { ClientProxy } from "@nestjs/microservices";
-import { lastValueFrom, timeout } from "rxjs";
-import { ApiConsumes, ApiTags } from "@nestjs/swagger";
-import { StudentPatterns } from "../../common/enums/student.events";
-import { ServiceResponse } from "../../common/interfaces/serviceResponse.interface";
-import { CreateStudentDto } from "../../common/dtos/student.dto";
-import { SwaggerConsumes } from "src/common/enums/swagger-consumes.enum";
+import { Body, Controller, Get, HttpException, Inject, InternalServerErrorException, Post } from '@nestjs/common';
+import { Services } from '../../common/enums/services.enum';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom, timeout } from 'rxjs';
+import { ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { StudentPatterns } from '../../common/enums/student.events';
+import { ServiceResponse } from '../../common/interfaces/serviceResponse.interface';
+import { CreateStudentDto } from '../../common/dtos/student.dto';
+import { SwaggerConsumes } from 'src/common/enums/swagger-consumes.enum';
 
-@Controller("student")
-@ApiTags("Student")
+@Controller('student')
+@ApiTags('Student')
 export class StudentController {
   constructor(@Inject(Services.STUDENT) private readonly studentServiceClient: ClientProxy) {}
 
@@ -17,7 +17,7 @@ export class StudentController {
     try {
       return await lastValueFrom(this.studentServiceClient.send(StudentPatterns.checkConnection, {}).pipe(timeout(5000)));
     } catch (error) {
-      throw new InternalServerErrorException("Student service is not connected");
+      throw new InternalServerErrorException('Student service is not connected');
     }
   }
 
@@ -27,14 +27,14 @@ export class StudentController {
     await this.checkConnection();
 
     const data: ServiceResponse = await lastValueFrom(
-      this.studentServiceClient.send(StudentPatterns.createStudent, {}).pipe(timeout(5000))
+      this.studentServiceClient.send(StudentPatterns.createStudent, {}).pipe(timeout(5000)),
     );
 
     if (data.error) throw new HttpException(data.message, data.status);
 
     return data;
   }
-  @Get("hello")
+  @Get('hello')
   async getHello() {
     await this.checkConnection();
 
