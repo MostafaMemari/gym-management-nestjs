31f0e7e19dd9aabc55796e313f549c40620ee189
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-02-09 23:10:10 +0330

    feat: add shared dto file and add pagination dto in file

---
+++ gateway/src/common/dtos/shared.dto.ts
@@ -0,0 +1,19 @@
+import { ApiPropertyOptional } from "@nestjs/swagger"
+import { Transform } from "class-transformer"
+import { IsNumber, IsOptional, IsPositive } from "class-validator"
+
+export class PaginationDto {
+    @ApiPropertyOptional()
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    @Transform(({ value }) => +value)
+    page: number = 1
+
+    @ApiPropertyOptional()
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    @Transform(({ value }) => +value)
+    count: number = 20
+}
--- gateway/src/common/dtos/user.dto.ts
+++ gateway/src/common/dtos/user.dto.ts
@@ -1,22 +1,6 @@
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger"
-import { Transform } from "class-transformer"
-import { IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from "class-validator"
-
-export class PaginationDto {
-    @ApiPropertyOptional()
-    @IsNumber()
-    @IsPositive()
-    @IsOptional()
-    @Transform(({ value }) => +value)
-    page: number = 1
-
-    @ApiPropertyOptional()
-    @IsNumber()
-    @IsPositive()
-    @IsOptional()
-    @Transform(({ value }) => +value)
-    count: number = 20
-}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty, IsString } from "class-validator"
+import { PaginationDto } from "./shared.dto"
 
 export class SearchDto extends PaginationDto {
     @IsNotEmpty()
