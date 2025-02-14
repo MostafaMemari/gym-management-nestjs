e7fd300284a179e30836f2408c7e2a9b3f91447e
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-02-14 13:17:38 +0330

    feat: merge conflicts

--- gateway/src/common/dtos/user.dto.ts
+++ gateway/src/common/dtos/user.dto.ts
@@ -1,8 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
-import { PaginationDto } from "./shared.dto"
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
 import { PaginationDto } from './shared.dto';
 
 export class SearchDto extends PaginationDto {
