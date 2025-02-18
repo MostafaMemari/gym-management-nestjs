57a45719fccc6d67625de958168763858cd41906
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-02-19 00:48:50 +0330

    feat: add cache enum file

---
+++ user/src/common/enums/cache.enum.ts
@@ -0,0 +1,9 @@
+export enum CachePatterns {
+    UsersList = "users*",
+    SearchUsersList = "search_users*"
+}
+
+export enum CacheKeys {
+    Users = "users",
+    SearchUsers = "search_users"
+}
