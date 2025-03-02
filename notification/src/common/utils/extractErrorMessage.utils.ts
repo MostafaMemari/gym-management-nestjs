41d8909d4ab7f50c3860e5e568cc6dd769a4ea10
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-02 15:13:39 +0330

    feat: add extractErrorMessage file and fn

---
+++ notification/src/common/utils/extractErrorMessage.utils.ts
@@ -0,0 +1,6 @@
+export const extractErrorMessage = (error: any, defaultMessage?: string) => {
+    if (typeof error == 'string') return error;
+    else if (typeof error == 'object' && error?.message) return error.message;
+
+    return defaultMessage || '';
+};
