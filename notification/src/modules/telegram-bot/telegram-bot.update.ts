005ba60038f952a4a42b5a62a83aa98991a9aba6
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-03 18:02:30 +0330

    feat: add start set national code method

--- notification/src/modules/telegram-bot/telegram-bot.update.ts
+++ notification/src/modules/telegram-bot/telegram-bot.update.ts
@@ -1,10 +1,58 @@
-import { Ctx, Start, Update } from 'nestjs-telegraf';
-import { Context } from 'telegraf';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Action, Command, Ctx, Start, Update } from 'nestjs-telegraf';
+import { Context, Markup } from 'telegraf';
+import { TelegramBot } from './telegram-bot.schema';
+import { ConflictException } from '@nestjs/common';
 
 @Update()
 export class TelegramBotUpdate {
+  constructor(@InjectModel(TelegramBot.name) private readonly telegramBotModel: Model<TelegramBot>) {}
+
   @Start()
   async startBot(@Ctx() ctx: Context) {
-    await ctx.reply('hello sajad how are you?');
+    await ctx.reply(
+      `سلام ${ctx.from.first_name} به ربات اعلانات آکادمی یاری خوش آمدید.`,
+      Markup.inlineKeyboard([Markup.button.callback('ثبت کد ملی', `set_national`)]),
+    );
+  }
+
+  @Action('set_national')
+  async setNationalCode(@Ctx() ctx: Context) {
+    await ctx.reply(`
+لطفا کد ملی خود را وارد نمایید
+
+راهنمایی: 
+/set_national 0010350899
+      `);
+  }
+
+  @Command('set_national')
+  async setNational(@Ctx() ctx: Context) {
+    try {
+      const nationalCode = ctx.text?.split(' ')?.[1] || '';
+
+      if (!nationalCode.match(/^[0-9]{10}$/)) {
+        await ctx.reply('کد ملی نامعتبر می باشد.');
+        return;
+      }
+
+      const existingNationalCode = await this.telegramBotModel.findOne({ nationalCode });
+
+      if (existingNationalCode) throw new ConflictException('کد ملی از قبل ثبت شده است.');
+
+      //TODO: Check and find national code from student service
+
+      await this.telegramBotModel.create({
+        nationalCode,
+        telegramId: ctx.from.id,
+      });
+
+      await ctx.reply('کد ملی شما با موفقیت ثبت شد.✅ زین پس اعلانات مربوطه برای شما ارسال خواهد شد');
+
+      await ctx.reply('کد ملی شما ثبت شد');
+    } catch (error) {
+      await ctx.reply(error.message);
+    }
   }
 }
