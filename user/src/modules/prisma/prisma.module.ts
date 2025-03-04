04026aa6b3d8a09d9e296571883451542d933bf6
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-04 19:27:43 +0330

    feat: add prisma to module folder

---
+++ user/src/modules/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { CacheService } from '../cache/cache.service';
+
+@Global()
+@Module({
+  providers: [PrismaService, CacheService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
---
+++ user/src/modules/prisma/prisma.service.ts
@@ -0,0 +1,38 @@
+import { forwardRef, Inject, Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { CacheService } from '../cache/cache.service';
+import { CachePatterns } from '../common/enums/cache.enum';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(@Inject(forwardRef(() => CacheService)) private readonly cacheService: CacheService) {
+    super();
+  }
+
+  private readonly logger = new Logger('PrismaService');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected successfully.');
+    this.useMiddleware();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log('Disconnected from database.');
+  }
+
+  useMiddleware(): void {
+    this.$use(async (params, next) => {
+      const { action } = params;
+
+      if (['create', 'update', 'delete'].includes(action)) {
+        //* Clear user cache on create, update, or delete
+        await this.cacheService.delByPattern(CachePatterns.UsersList);
+        await this.cacheService.delByPattern(CachePatterns.SearchUsersList);
+      }
+
+      return next(params);
+    });
+  }
+}
