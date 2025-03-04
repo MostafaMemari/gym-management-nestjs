7ce4691ed05807031b9e5642a2ba8c1ca97f08a6
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-04 22:01:21 +0330

    feat: add create wallet method

--- gateway/src/app/controllers/user/wallet.controller.ts
+++ gateway/src/app/controllers/user/wallet.controller.ts
@@ -1,6 +1,39 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Inject, Post } from "@nestjs/common";
+import { AuthDecorator } from "../../../common/decorators/auth.decorator";
+import { Roles } from "../../../common/decorators/role.decorator";
+import { Role } from "../../../common/enums/role.enum";
+import { handleError, handleServiceResponse } from "src/common/utils/handleError.utils";
+import { Services } from "src/common/enums/services.enum";
+import { checkConnection } from "src/common/utils/checkConnection.utils";
+import { ClientProxy } from "@nestjs/microservices";
+import { lastValueFrom, timeout } from "rxjs";
+import { WalletPatterns } from "src/common/enums/wallet.events";
+import { GetUser } from "src/common/decorators/get-user.decorator";
+import { User } from "src/common/interfaces/user.interface";
+import { ApiTags } from "@nestjs/swagger";
 
-@Controller()
+@Controller("wallet")
+@ApiTags("wallet")
+@AuthDecorator()
 export class WalletController {
-    
+
+    private timeout = 5000
+
+    constructor(@Inject(Services.USER) private readonly userServiceClient: ClientProxy) { }
+
+    @Post()
+    @Roles(Role.ADMIN_CLUB)
+    async create(@GetUser() user: User) {
+        try {
+            await checkConnection(Services.USER, this.userServiceClient)
+
+            const walletData = { userId: user.id }
+
+            const data = await lastValueFrom(this.userServiceClient.send(WalletPatterns.CreateWallet, walletData).pipe(timeout(this.timeout)))
+
+            return handleServiceResponse(data)
+        } catch (error) {
+            handleError(error, "Failed to create wallet", Services.USER)
+        }
+    }
 }
