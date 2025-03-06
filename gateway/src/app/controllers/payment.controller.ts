4363a4771ca81cac2a7ab73c6d07fff79f1b6582
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-06 15:58:12 +0330

    feat: add transactions methods

--- gateway/src/app/controllers/payment.controller.ts
+++ gateway/src/app/controllers/payment.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller, Get, Inject, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { Services } from '../../common/enums/services.enum';
 import { AuthDecorator } from '../../common/decorators/auth.decorator';
 import { PaymentDto } from '../../common/dtos/payment.dto';
-import { GetUser } from 'src/common/decorators/get-user.decorator';
-import { User } from 'src/common/interfaces/user.interface';
-import { handleError, handleServiceResponse } from 'src/common/utils/handleError.utils';
-import { SwaggerConsumes } from 'src/common/enums/swagger-consumes.enum';
+import { GetUser } from '../../common/decorators/get-user.decorator';
+import { User } from '../../common/interfaces/user.interface';
+import { handleError, handleServiceResponse } from '../../common/utils/handleError.utils';
+import { SwaggerConsumes } from '../../common/enums/swagger-consumes.enum';
 import { lastValueFrom, timeout } from 'rxjs';
-import { checkConnection } from 'src/common/utils/checkConnection.utils';
-import { PaymentPatterns } from 'src/common/enums/payment.events';
+import { checkConnection } from '../../common/utils/checkConnection.utils';
+import { PaymentPatterns } from '../../common/enums/payment.events';
+import { Roles } from '../../common/decorators/role.decorator';
+import { Role } from '../../common/enums/role.enum';
 
 @Controller('payment')
 @ApiTags('payment')
@@ -18,7 +20,7 @@ import { PaymentPatterns } from 'src/common/enums/payment.events';
 export class PaymentController {
   private timeout = 5000;
 
-  constructor(@Inject(Services.PAYMENT) private readonly paymentServiceClient: ClientProxy) {}
+  constructor(@Inject(Services.PAYMENT) private readonly paymentServiceClient: ClientProxy) { }
 
   @Post()
   @ApiConsumes(SwaggerConsumes.Json, SwaggerConsumes.UrlEncoded)
@@ -48,7 +50,7 @@ export class PaymentController {
       const verifyData = {
         authority,
         status,
-        frontendUrl: 'http://localhost:4000/api/v1/payment/success',
+        frontendUrl: 'http://localhost:4000/api/v1/payment/success', //TODO: Add frontend url to env
       };
 
       const data = await lastValueFrom(this.paymentServiceClient.send(PaymentPatterns.VerifyPayment, verifyData).pipe(timeout(this.timeout)));
@@ -59,8 +61,44 @@ export class PaymentController {
     }
   }
 
-  @Get('success')
-  successResponse() {
-    return 'payment successfully';
+  @Get('my/transactions')
+  async getMyTransactions(@GetUser() user: User) {
+    try {
+      await checkConnection(Services.PAYMENT, this.paymentServiceClient);
+
+      const data = await lastValueFrom(this.paymentServiceClient.send(PaymentPatterns.GetUserTransactions, { userId: user.id }).pipe(timeout(this.timeout)));
+
+      return handleServiceResponse(data);
+    } catch (error) {
+      handleError(error, 'Failed to get transactions', Services.PAYMENT);
+    }
+  }
+
+  @Get('transactions')
+  @Roles(Role.SUPER_ADMIN)
+  async getTransactions() {
+    try {
+      await checkConnection(Services.PAYMENT, this.paymentServiceClient);
+
+      const data = await lastValueFrom(this.paymentServiceClient.send(PaymentPatterns.GetTransactions, {}).pipe(timeout(this.timeout)));
+
+      return handleServiceResponse(data);
+    } catch (error) {
+      handleError(error, 'Failed to get transactions', Services.PAYMENT);
+    }
+  }
+
+  @Get('transaction/:id')
+  @Roles(Role.SUPER_ADMIN)
+  async getOneTransaction(@Param('id', ParseIntPipe) id: number) {
+    try {
+      await checkConnection(Services.PAYMENT, this.paymentServiceClient);
+
+      const data = await lastValueFrom(this.paymentServiceClient.send(PaymentPatterns.GetOneTransaction, { transactionIid: id }).pipe(timeout(this.timeout)));
+
+      return handleServiceResponse(data);
+    } catch (error) {
+      handleError(error, 'Failed to get transaction', Services.PAYMENT);
+    }
   }
 }
--- gateway/src/common/enums/payment.events.ts
+++ gateway/src/common/enums/payment.events.ts
@@ -1,4 +1,7 @@
 export enum PaymentPatterns {
   CreateGatewayUrl = 'create_gateway_url',
   VerifyPayment = 'verify_payment',
+  GetUserTransactions = 'get_user_transactions',
+  GetTransactions = 'get_transactions',
+  GetOneTransaction = 'get_one_transaction'
 }
