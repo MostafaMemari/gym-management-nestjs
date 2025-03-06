d9823fa9c20014df08831a4fd7fe1c7a03dea40c
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-06 20:37:41 +0330

    feat: add middleware to prisma

--- payment/src/modules/prisma/prisma.module.ts
+++ payment/src/modules/prisma/prisma.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { CacheService } from '../cache/cache.service';
 
 @Module({
-  providers: [PrismaService],
+  providers: [PrismaService, CacheService],
   exports: [PrismaService],
 })
 export class PrismaModule {}
--- payment/src/modules/prisma/prisma.service.ts
+++ payment/src/modules/prisma/prisma.service.ts
@@ -1,17 +1,34 @@
 import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { CacheService } from '../cache/cache.service';
+import { CachePatterns } from '../../common/enums/cache.enum';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   logger = new Logger(PrismaService.name);
 
+  constructor(private readonly cacheService: CacheService) {
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
+    this.useMiddleware();
     this.logger.log('Connected to database.');
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
     this.logger.log('Disconnected from database.');
+  }
+
+  useMiddleware() {
+    this.$use(async (params, next) => {
+      if (['create', 'update', 'delete'].includes(params.action)) {
+        await this.cacheService.delByPattern(CachePatterns.TransactionList);
+      }
+
+      return await next(params);
+    });
   }
 }
