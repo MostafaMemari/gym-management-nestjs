3ac0eb7762033ed47cdeceb13c5a8b925b5ab2fb
MostafaMemari <m.memari711@gmail.com>
2025-03-16 00:47:13 +0330

    ✨ feat: refactor attendance subscriber to use AttendanceSessionEntity for improved session handling

--- club/src/modules/attendance/subscribers/attendance.subscriber.ts
+++ club/src/modules/attendance/subscribers/attendance.subscriber.ts
@@ -3,29 +3,29 @@ import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource, EntitySubscriberInterface, InsertEvent, UpdateEvent, RemoveEvent } from 'typeorm';
 
 import { CachePatterns } from '../enums/cache.enum';
-import { AttendanceEntity } from '../entities/attendance.entity';
+import { AttendanceSessionEntity } from '../entities/attendance-sessions.entity';
 
 import { CacheService } from '../../cache/cache.service';
 
 @Injectable()
-export class AttendanceSubscriber implements EntitySubscriberInterface<AttendanceEntity> {
+export class AttendanceSubscriber implements EntitySubscriberInterface<AttendanceSessionEntity> {
   constructor(@InjectDataSource() private readonly dataSource: DataSource, private readonly cacheService: CacheService) {
     this.dataSource.subscribers.push(this);
   }
 
   listenTo() {
-    return AttendanceEntity;
+    return AttendanceSessionEntity;
   }
 
-  async afterInsert(event: InsertEvent<AttendanceEntity>) {
+  async afterInsert(event: InsertEvent<AttendanceSessionEntity>) {
     await this.clearCache();
   }
 
-  async afterUpdate(event: UpdateEvent<AttendanceEntity>) {
+  async afterUpdate(event: UpdateEvent<AttendanceSessionEntity>) {
     await this.clearCache();
   }
 
-  async afterRemove(event: RemoveEvent<AttendanceEntity>) {
+  async afterRemove(event: RemoveEvent<AttendanceSessionEntity>) {
     await this.clearCache();
   }
 
