116af680702e44b14c95b5151d269dc709867ef6
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-16 09:46:19 +0330

    feat: add user service

--- notification/src/modules/notification/notification.module.ts
+++ notification/src/modules/notification/notification.module.ts
@@ -6,12 +6,24 @@ import envConfig from '../../configs/env.config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Notification, NotificationSchema } from './notification.schema';
 import { TelegramBotModule } from '../telegram-bot/telegram-bot.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { Services } from 'src/common/enums/services.enum';
 
 @Module({
   imports: [
     ConfigModule.forRoot(envConfig()),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema }]),
+    ClientsModule.register([
+      {
+        name: Services.USER,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: process.env.RABBITMQ_USER_SERVICE_QUEUE,
+        },
+      },
+    ]),
     //TelegramBotModule,
   ],
   controllers: [NotificationController],
