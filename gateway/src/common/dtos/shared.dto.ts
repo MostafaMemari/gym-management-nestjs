1fd46bf1a11183ee715c4e82e67c3f6cd3facd25
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-04-07 18:11:45 +0330

    feat: add maximum for take pagination and add swagger properties

--- gateway/src/common/dtos/shared.dto.ts
+++ gateway/src/common/dtos/shared.dto.ts
@@ -1,19 +1,29 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, Max } from 'class-validator';
 
 export class PaginationDto {
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    type: 'number',
+    required: false,
+    nullable: true,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   @Transform(({ value }) => Number(value) || 1)
   page: number = 1;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    type: 'number',
+    maximum: 100,
+    required: false,
+    nullable: true,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
+  @Max(100)
   @Transform(({ value }) => Number(value) || 20)
   take: number = 20;
 }
