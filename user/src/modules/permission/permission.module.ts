c29429d2046745a6a423b27955f68b2768c5626f
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-04-14 13:25:50 +0330

    fix: dependency injection error

--- user/src/modules/permission/permission.module.ts
+++ user/src/modules/permission/permission.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import { PermissionController } from './permission.controller';
+import { PermissionRepository } from './permission.repository';
+import { CacheService } from '../cache/cache.service';
 
 @Module({
   controllers: [PermissionController],
-  providers: [PermissionService],
+  providers: [PermissionService, PermissionRepository, CacheService],
 })
 export class PermissionModule {}
