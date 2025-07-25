c76d1b50f88357f5fdc33a816a01e9365db0d0a4
MostafaMemari <m.memari711@gmail.com>
2025-07-25 16:55:20 +0330

    fix: update username validation regex in SignupDto and UpdateUserDto to enforce specific format

--- gateway/src/common/dtos/auth-service/auth.dto.ts
+++ gateway/src/common/dtos/auth-service/auth.dto.ts
@@ -7,7 +7,7 @@ export class SignupDto {
   @IsString()
   @MaxLength(100)
   @MinLength(3)
-  @Matches(/^(?![0-9])[a-zA-Z0-9_-]{3,20}$/, {
+  @Matches(/^09\d{9}$/, {
     message: 'username is invalid',
   })
   @ApiProperty({
